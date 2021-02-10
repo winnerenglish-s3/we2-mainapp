deca542a3a95ff5c46352d8c065192d6a95c1ce0
MMasterZ <soulhunz@hotmail.com>
2021-02-10 11:41:12 +0700

    เพิ่ม themeSync

--- src/layouts/PracticeLayout.vue
+++ src/layouts/PracticeLayout.vue
@@ -2,15 +2,20 @@
   <q-layout view="lHh Lpr lFf">
     <!-- Layout Page -->
     <q-page-container>
-      <router-view />
+      <router-view :themeSync="themeSync" />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup(props) {
+    const themeSync = ref(1);
+
+    return { themeSync };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
