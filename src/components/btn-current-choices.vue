e687a937929e41795fa1c427f2da1efaef908950
MMasterZ <soulhunz@hotmail.com>
2021-02-24 21:56:55 +0700

    update btn current question

---
+++ src/components/btn-current-choices.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="row">
+      <div v-for="i in practiceData.totalQuestion">
+        <q-img
+          class="brx"
+          width="45px"
+          src="../../public/images/question-default.png"
+        ></q-img>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    practiceData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
