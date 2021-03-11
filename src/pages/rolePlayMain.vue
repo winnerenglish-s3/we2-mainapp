7a25a6aa2af051a5e3aeba5aa939d6d6d7a27453
Tanin <ber_zebub@hotmail.com>
2021-03-11 12:15:44 +0700

    Create Role Play Route

---
+++ src/pages/rolePlayMain.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="bg-roleplay">
+    <app-bar
+      :isShowPause="true"
+      :themeSync="themeSync"
+      :instructionData="instructionData"
+    ></app-bar>
+  </q-page>
+</template>
+
+<script>
+import appBar from "../components/app-bar";
+import { useQuasar } from "quasar";
+import { ref, onMounted, reactive } from "vue";
+import { useRouter, useRoute } from "vue-router";
+export default {
+  components: {
+    appBar,
+  },
+  props: {
+    themeSync: {
+      type: Number,
+      default: 0,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bg-roleplay {
+  background-image: url("../../public/images/bg-character.png");
+}
+</style>
